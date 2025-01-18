a6c520cd09ba991a188b9489169f9be75aeb2ed1
abinercosta-cl <abinerclcosta@gmail.com>
2025-01-17 23:18:35 -0300

    add lucide icons and icons of the menu navegation

--- spotfy-clone-tailwind/src/app/page.tsx
+++ spotfy-clone-tailwind/src/app/page.tsx
@@ -1,19 +1,43 @@
+import { House, Search, Library } from "lucide-react";
+
 export default function Home() {
 	return (
 		<div className="h-screen flex flex-col">
 			<div className="flex flex-1">
 				<aside className="w-72 bg-zinc-950 p-6">
-					<nav>
-						<a href="">Home</a>
-						<a href="">Search</a>
-						<a href="">Your Library</a>
+					<div className="flex items-center gap-2">
+						<div className="w-3 h-3 bg-red-600 rounded-full"></div>
+						<div className="w-3 h-3 bg-yellow-600 rounded-full"></div>
+						<div className="w-3 h-3 bg-green-600 rounded-full"></div>
+					</div>
+					<nav className="space-y-4 mt-10">
+						<a
+							href=""
+							className="flex items-center gap-2 text-xs text-zinc-200"
+						>
+							<House />
+							Home
+						</a>
+						<a
+							href=""
+							className="flex items-center gap-2 text-xs text-zinc-200"
+						>
+							<Search />
+							Search
+						</a>
+						<a
+							href=""
+							className="flex items-center gap-2 text-xs text-zinc-200"
+						>
+							<Library />
+							Your Library
+						</a>
 					</nav>
 				</aside>
 				<main className="flex-1 p-6">main</main>
 			</div>
 			<footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-				{" "}
-				footer{" "}
+				footer
 			</footer>
 		</div>
 	);
