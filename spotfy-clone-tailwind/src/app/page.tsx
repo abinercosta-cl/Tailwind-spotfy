abb61bf0063db808cf1fff93468d8f9ddf757e2a
abinercosta-cl <abinerclcosta@gmail.com>
2025-01-19 12:05:34 -0300

    create list of librarys and add icons the album Judas

--- spotfy-clone-tailwind/src/app/page.tsx
+++ spotfy-clone-tailwind/src/app/page.tsx
@@ -1,4 +1,10 @@
-import { House, Search, Library } from "lucide-react";
+import {
+	House,
+	Search,
+	Library,
+	ChevronLeft,
+	ChevronRight,
+} from "lucide-react";
 
 export default function Home() {
 	return (
@@ -33,8 +39,83 @@ export default function Home() {
 							Your Library
 						</a>
 					</nav>
+					<nav className="mt-6 pt-6 border-t border-zinc-700 flex flex-col gap-2">
+						<a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+							Top Hits Brazil
+						</a>
+						<a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+							Treinaooo Massa
+						</a>
+						<a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+							Pagodeira
+						</a>
+						<a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+							Sertanejo Pesadoo
+						</a>
+					</nav>
 				</aside>
-				<main className="flex-1 p-6">main</main>
+				<main className="flex-1 p-6">
+					<div className="flex items-center gap-3">
+						<button className="rounded-full bg-black/40 p-1">
+							<ChevronLeft />
+						</button>
+						<button className="rounded-full bg-black/40 p-1">
+							<ChevronRight />
+						</button>
+					</div>
+					<h1 className="font-semi-bold text-3xl mt-10">Good Afternoon</h1>
+
+					<div className="grid grid-cols-3 gap-4">
+						<div className="bg-white/10 rounded">
+							<img
+								src="/judas_album_7mz.png"
+								width={80}
+								height={80}
+								alt="caveira branca"
+							/>
+						</div>
+						<div className="bg-white/10 rounded">
+							<img
+								src="/judas_album_7mz.png"
+								width={80}
+								height={80}
+								alt="caveira branca"
+							/>
+						</div>
+						<div className="bg-white/10 rounded">
+							<img
+								src="/judas_album_7mz.png"
+								width={80}
+								height={80}
+								alt="caveira branca"
+							/>
+						</div>
+						<div className="bg-white/10 rounded">
+							<img
+								src="/judas_album_7mz.png"
+								width={80}
+								height={80}
+								alt="caveira branca"
+							/>
+						</div>
+						<div className="bg-white/10 rounded">
+							<img
+								src="/judas_album_7mz.png"
+								width={80}
+								height={80}
+								alt="caveira branca"
+							/>
+						</div>
+						<div className="bg-white/10 rounded">
+							<img
+								src="/judas_album_7mz.png"
+								width={80}
+								height={80}
+								alt="caveira branca"
+							/>
+						</div>
+					</div>
+				</main>
 			</div>
 			<footer className="bg-zinc-800 border-t border-zinc-700 p-6">
 				footer
